b9165279451ca55a9215668c6e44f00a5230276d
Seiji Hiraki <19279032+hirakisp@users.noreply.github.com>
2020-04-23 10:08:06 +0900

    Update example1.c

--- src/example1.c
+++ src/example1.c
@@ -12,11 +12,11 @@ void example1_init(void)
 
 int example1_entry(void *ev)
 {
-	int ev_msg;
+	char ev_msg;
 	printf("%s \n", __func__);
 	ev_msg = 0xFF;
 
-	hsm_set_ev(HSM_EVENT_1, &ev_msg);
+	hsm_set_ev(HSM_EVENT_1, (char *)&ev_msg, 1);
 
 	return HSM_NOTRANS;
 }
@@ -31,7 +31,7 @@ int example1_event1(void *ev)
 {
 	printf("%s \n", __func__);
 
-	int * arg = (int *)(ev);
+	char * arg = (char *)(ev);
 
 	printf(" argment is 0x%02x\n", *arg);
 
